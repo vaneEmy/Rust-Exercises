c9577c28b7692176e927854773864f6da136be68
vaneEmy <vanvanemily@gmail.com>
2018-04-20 13:19:58 -0500

    Adding and remove elements from Vector d.

--- vectors.rs
+++ vectors.rs
@@ -2,5 +2,13 @@ fn  main() {
     let mut c = vec![5, 4, 3, 2, 1];
     c[0] = 1;
     c[1] = 2;
-    println!("{:?}", c);
+    println!("Vector a: {:?}", c);
+
+    let mut d: Vec<i32> = Vec::new();
+    d.push(1);
+    d.push(2);
+    d.pop();
+
+    println!("Vector d: {:?}", d);
+
 }
