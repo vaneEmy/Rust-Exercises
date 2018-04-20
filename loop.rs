73612c1c47ae9ef568021e6d3f4e3160822386ee
vaneEmy <vanvanemily@gmail.com>
2018-04-20 00:09:14 -0500

    Adding a simple example of Outer break.

--- loop.rs
+++ loop.rs
@@ -14,6 +14,21 @@ fn main(){
        }
        println!("Current Value: {}", a);
        a += 1;
+    }
 
-   }
+    // Outer break
+    let mut b1 = 1;
+    'outer_loop: loop{
+        let mut b2 = 1;
+        'inner_loop: loop{
+            println!("Current Value: [{}] [{}]", b1, b2);
+            if b1 == 2 && b2 == 2{
+                break 'outer_loop;
+            } else if b2 == 5{
+                break;
+            }
+            b2 += 1;
+        }
+        b1 += 1;
+    }
 }
