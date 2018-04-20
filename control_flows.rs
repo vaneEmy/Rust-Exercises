be84ab806f8342869054f80af4ded23fd8fc5bad
vaneEmy <vanvanemily@gmail.com>
2018-04-19 22:24:27 -0500

    Optimistic refactored code.

--- control_flows.rs
+++ control_flows.rs
@@ -1,12 +1,11 @@
 fn main(){
   let team_size = 7;
-  let team_size_in_text;
-  if team_size < 5{
-     team_size_in_text = "Small";
-  } else if team_size < 10{
-    team_size_in_text = "Medium";
+  let team_size_in_text = if team_size < 5{
+    "Small"
+    } else if team_size < 10{
+    "Medium"
   }else{
-    team_size_in_text = "Large";
-  }
-  println!("Current team size: {}", team_size_in_text);
+    "Large"
+  };
+  println!("Current team size : {}", team_size_in_text);
 }
