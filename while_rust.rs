1eaefb39e8326c3d8927ad27dbcf90dfe1982a67
vaneEmy <vanvanemily@gmail.com>
2018-04-19 22:41:15 -0500

    Adding another example for while but using break and continue.

--- while_rust.rs
+++ while_rust.rs
@@ -5,4 +5,18 @@ fn main(){
         a += 1;
     }
 
+    let mut b = 0;
+    while b < 5{    
+        if b == 0{
+            println!("Skip value: {}", b);
+            b += 1;
+            continue;
+        } else if b == 2{
+            println!("Break at: {}", b);
+            break;
+        }
+        println!("Current value: {}", b);
+        b += 1;
+    }
+
 }
