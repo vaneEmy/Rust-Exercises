5fa147fbc90d412a56079c125838603a24476302
vaneEmy <vanvanemily@gmail.com>
2018-04-19 22:53:05 -0500

    Adding a simple example for loiop using break and continue.

--- loop.rs
+++ loop.rs
@@ -1,5 +1,19 @@
 fn main(){
-    loop{
-        println!("Loop forever!");
-    }
+    //loop{
+    //    println!("Loop forever!");
+   // }
+   let mut a = 0;
+   loop{
+       if a == 0{
+           println!("Skip Value: {}", a);
+            a += 1;
+            continue;
+       } else if a == 2{
+           println!("Break At: {}", a);
+           break;
+       }
+       println!("Current Value: {}", a);
+       a += 1;
+
+   }
 }
