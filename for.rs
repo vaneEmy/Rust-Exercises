7f70c3f9a483a811adcbfc11ce77c014aa001e7e
vaneEmy <vanvanemily@gmail.com>
2018-04-20 00:20:38 -0500

    Creating a simple example for Outer break in for.

--- for.rs
+++ for.rs
@@ -3,6 +3,8 @@ fn main(){
         println!("Current value: {}", a);
     }
 
+    println!("------ Break and continue ------");
+
     for b in 0..6{
         if b == 0{
             println!("Skip value: {}", b);
@@ -12,5 +14,14 @@ fn main(){
             break;
         }
         println!("Current value: {}", b);
+    }
+
+    println!("------ Outer break-------");
+
+    'outer_for: for c1 in 1..6{
+        'inner_for: for c2 in 1..6{
+            println!("Current value: [{}] [{}]", c1, c2);
+            if c1 == 2 && c2 == 2 { break 'outer_for}
+        }
     }
 }
