9332553ef02a0fa1fc76b74a3228181a0ecaac30
vaneEmy <vanvanemily@gmail.com>
2018-04-20 00:13:27 -0500

    Adding an example forthe use of break and continue in for.

--- for.rs
+++ for.rs
@@ -2,4 +2,15 @@ fn main(){
     for a in 0..10{
         println!("Current value: {}", a);
     }
+
+    for b in 0..6{
+        if b == 0{
+            println!("Skip value: {}", b);
+            continue;
+        }else if b == 2{
+            println!("Break At: {}", b);
+            break;
+        }
+        println!("Current value: {}", b);
+    }
 }
