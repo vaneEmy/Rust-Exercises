f2baeb1404cb61bc312fd038eb7dccbd6359dfac
vaneEmy <vanvanemily@gmail.com>
2018-04-24 12:38:49 -0500

    Adding test annotation.

--- greetings/src/lib.rs
+++ greetings/src/lib.rs
@@ -1,3 +1,8 @@
-pub fn hello(){
-    println!("Hello, world!");
+pub fn hello() -> String{
+    ("Hello, world!").to_string()
+}
+
+#[test]
+fn test_hello() {
+    assert_eq!(hello(), "Hello, wolrd!");
 }
