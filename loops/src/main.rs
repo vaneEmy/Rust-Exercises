bf11edcdb91307d272341db4fb496d9cff382b87
vaneEmy <vanvanemily@gmail.com>
2018-04-26 13:50:44 -0500

    Looping through each element of a collection using a for loop.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-
-        println!("The value is: {}", a[index]);
-
-        index = index + 1;
+    for element in a.iter(){
+        println!("The value is: {}", element);
     }
 }
