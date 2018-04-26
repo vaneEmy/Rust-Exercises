c09956b869045e5ce07827e92c1b471356e2a6ec
vaneEmy <vanvanemily@gmail.com>
2018-04-26 13:36:09 -0500

    Adding multiple if conditions.

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let number = 7;
+    let number = 6;
 
-    if number < 5 {
-        println!("Condition was true");
+    if number % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0{
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0{
+        println!("Number is divisible by 2");
     } else {
-        println!("Condition was false");
+        println!("Number is not divisible by 4, 3, or 2");
     }
 }
