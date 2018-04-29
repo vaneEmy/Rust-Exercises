68148f6c4fb27776ccab306b747aed8a72768bb0
vaneEmy <vanvanemily@gmail.com>
2018-04-28 23:08:08 -0500

    Adding a mutable referneces.

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s = String::from("hello");
 
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    change(&mut s);
 }
 
-fn calculate_length(s: &String) -> usize{
-    s.len()
+fn change(some_string: &mut String){
+    some_string.push_str(", world");
 }
