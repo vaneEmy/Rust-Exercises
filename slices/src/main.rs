2c5de2383f784550b94e1c9baf664b990f1c174f
vaneEmy <vanvanemily@gmail.com>
2018-05-01 16:44:02 -0500

    Solving the problem using slice.

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,19 +1,18 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s);
-    
+    let word = first_word(&my_string[..]);
+
     println!("Word: {}", word);
-    s.clear();
 }
-fn first_word(s: &String) -> usize{
+fn first_word(s: &str) -> &str{
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate(){
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }  
     }
 
-    s.len()
+    &s[..]
 }    
