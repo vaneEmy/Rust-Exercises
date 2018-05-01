89fdc97d195a0a3b8d77b989757ffac881d4f7e3
vaneEmy <vanvanemily@gmail.com>
2018-05-01 17:30:04 -0500

    Creating an instance of the User struct.

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -6,4 +6,10 @@ struct User{
 }
 
 fn main() {
+    let user1 = User{
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
 }
