afa62f13b248004b95dd9ded90e8d67deace46a2
vaneEmy <vanvanemily@gmail.com>
2018-05-01 18:22:44 -0500

    Creatin a new User instance using some of the values from user1.

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -9,6 +9,13 @@ fn main() {
     let email1 = String::from("anotheremail@example.com");
     let username1 = String::from("someoneusername123");
     let user1 = build_user(email1, username1);
+
+    let user2 = User{
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
 }
 
 fn build_user(email: String, username: String) -> User{
