d4c974a90cbad93330d401f6b200ee9fa7d59176
vaneEmy <vanvanemily@gmail.com>
2018-05-01 18:25:16 -0500

    Using struct update syntax to ser new email and username valaues for a User instance but use the rest of the values from the fields of the instance in the user1 variable.

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -13,8 +13,7 @@ fn main() {
     let user2 = User{
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
-        active: user1.active,
-        sign_in_count: user1.sign_in_count,
+        ..user1
     };
 }
 
