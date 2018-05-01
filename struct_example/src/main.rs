bc3ef0651d4900aad73f7fff29d1481264e1db95
vaneEmy <vanvanemily@gmail.com>
2018-05-01 17:43:39 -0500

    A build user function that takes an email and username and returns a User instance.

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -6,12 +6,16 @@ struct User{
 }
 
 fn main() {
-    let mut user1 = User{
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+    let email1 = String::from("anotheremail@example.com");
+    let username1 = String::from("someoneusername123");
+    let user1 = build_user(email1, username1);
+}
 
-    user1.email = String::from("anotheremail@example.com");
+fn build_user(email: String, username: String) -> User{
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,   
+    }
 }
