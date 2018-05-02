7ca9c242635a5ec9f2f69eb67d52afda2fe1bd7a
vaneEmy <vanvanemily@gmail.com>
2018-05-01 19:49:00 -0500

    Defining an area method on the Rectangle struct.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,14 +4,15 @@ struct Rectangle{
     height: u32,
 }
 
+impl Rectangle{
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("rect1 is {:#?}", rect1);
     println!("The area of the rectangle is {} square pixels.",
-            area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32{
-   rectangle.width * rectangle.height
+            rect1.area());
 }
