95e8de0a6c1a2ebaca1a4320949009052e4afc99
vaneEmy <vanvanemily@gmail.com>
2018-05-07 22:00:41 -0500

    We attach data to each variant of the enum directly, so there is no need for an extra struct.

--- ip_addresses/src/main.rs
+++ ip_addresses/src/main.rs
@@ -1,19 +1,10 @@
-enum IpAddrKind{
-    V4,
-    V6,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
-struct IpAddr{
-    kind: IpAddrKind,
-    address: String,
-}
 fn main() {
-    let home = IpAddr{
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home =  IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
 }
