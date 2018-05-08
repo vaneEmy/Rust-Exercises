6e7939b2dd9390bea05711c5775ebd253a85897c
vaneEmy <vanvanemily@gmail.com>
2018-05-07 23:40:14 -0500

    Extracting the contents of the client module but leaving the declaration in src/lib.rs

---
+++ communicator/src/lib.rs
@@ -0,0 +1,19 @@
+mod client;
+
+mod network{
+    fn connect(){
+    } 
+
+    mod server{
+        fn connect(){
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
