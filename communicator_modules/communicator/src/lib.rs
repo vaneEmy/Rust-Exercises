16ee4608402eb9be5ddd807192082335e0fe8e6d
vaneEmy <vanvanemily@gmail.com>
2018-05-10 00:47:02 -0500

    Adding publc privacity to server module.

--- communicator_modules/communicator/src/lib.rs
+++ communicator_modules/communicator/src/lib.rs
@@ -3,9 +3,9 @@ pub mod client;
 pub mod network;
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+mod test{
+    #[test] 
+    fn it_workd(){
         assert_eq!(2 + 2, 4);
     }
 }
--- communicator_modules/communicator/src/network/mod.rs
+++ communicator_modules/communicator/src/network/mod.rs
@@ -1,5 +1,5 @@
 pub fn connect(){
 
-} 
+}
 
 pub mod server;
