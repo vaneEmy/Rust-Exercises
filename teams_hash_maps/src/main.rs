43a580f6b2ef4579a4fddc09190e5d632f753e98
vaneEmy <vanvanemily@gmail.com>
2018-05-10 19:21:27 -0500

    Creating a hash map from a list of teams and a list of scores.

--- teams_hash_maps/src/main.rs
+++ teams_hash_maps/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
-    scores.insert(String::from("blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let teams = vec![String::from("blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
 }
