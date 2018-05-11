e7b3759e218ec371e46d9952ffd4e21df035a5aa
vaneEmy <vanvanemily@gmail.com>
2018-05-10 19:25:43 -0500

    Showing that keys and vales are owned by the hash map once ther're inserted.

--- teams_hash_maps/src/main.rs
+++ teams_hash_maps/src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 
-fn main() {
-    let teams = vec![String::from("blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
-    let scores: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+fn main() { 
+    let field_name = String::from("Favorite Color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 }
