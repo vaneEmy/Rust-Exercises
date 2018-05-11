177bca9bf35ba395c24c92aa560b17430733bed5
vaneEmy <vanvanemily@gmail.com>
2018-05-10 20:21:02 -0500

    Counting ocurrences of words using a hash map that stores words and counts.

--- updating_hash_map/src/main.rs
+++ updating_hash_map/src/main.rs
@@ -1,9 +1,14 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-    println!("{:?}", scores);
+    let text = "Hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace(){
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
