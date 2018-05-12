f337c692a2e8eb9f7f9cdc278e58a5a85c55f374
vaneEmy <vanvanemily@gmail.com>
2018-05-11 21:41:57 -0500

    Creating a function that returns errors to the calling code using match.

--- error_handling_result/src/main.rs
+++ error_handling_result/src/main.rs
@@ -1,6 +1,23 @@
+use std::io::Read;
 use std::fs::File;
+use std::io;
 
 fn main() {
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
 
+}
+
+fn read_username_from_file() -> Result<String, io::Error>{
+    let f = File::open("hello.txt");
+
+    let mut f = match f{
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s){
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
