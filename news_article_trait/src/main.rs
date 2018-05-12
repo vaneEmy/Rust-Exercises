78ffd9c707f84ddd87ad31d6356c8879beabc04c
vaneEmy <vanvanemily@gmail.com>
2018-05-12 01:43:07 -0500

    Implementing the Summary trait on the NewsArticles and Tweet types.

--- news_article_trait/src/main.rs
+++ news_article_trait/src/main.rs
@@ -1,9 +1,40 @@
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
 pub trait Summary {
     fn summarize(&self) -> String;
 }
 
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String{
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet{
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet{
+    fn summarize(&self) -> String{
+        format!("{} : {}", self.username, self.content)
+    }    
+}
 
 
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet{
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probabaly already know, people "),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
