6657b28fcebcb486635d472a501e374cdfea8bf6
vaneEmy <vanvanemily@gmail.com>
2018-05-11 21:27:33 -0500

    Adding a unwrap method.

--- error_handling_result/src/main.rs
+++ error_handling_result/src/main.rs
@@ -1,12 +1,6 @@
 use std::fs::File;
 
 fn main() {
-    let f = File::open("hello.txt");
+    let f = File::open("hello.txt").unwrap();
 
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error);
-        },
-    };
 }
