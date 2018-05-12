79a39f149047fb376b099b047b4acfebf284b96d
vaneEmy <vanvanemily@gmail.com>
2018-05-12 00:24:11 -0500

    Abstracted code to find the largest number in two list.

--- largest_number/src/main.rs
+++ largest_number/src/main.rs
@@ -1,24 +1,25 @@
-fn main() {
-    let number_list = vec![3, 50, 25, 100, 65];
-    
-    let mut largest = number_list[0];
+fn largest(list: &[i32]) -> i32{
+    let mut largest = list[0];
 
-    for number in number_list{
-        if number > largest{
-            largest = number;
+    for &item in list.iter(){
+        if item > largest{
+            largest = item;
         }
     }
-    println!("The largest number is {}", largest);
+    largest
+}
+
+fn main() {
+    let number_list = vec![3, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    
+    println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
     
-    let mut largest = number_list[0];
+    let result = largest(&number_list);
 
-    for number in number_list{
-        if number > largest{
-            largest = number;
-        }
-    }
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", result);
 
 }
