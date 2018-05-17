c985c6e5055b2af3645485f3085329771602eca0
vaneEmy <vanvanemily@gmail.com>
2018-05-17 13:37:51 -0500

    Showing custom Failure message.

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,4 +1,3 @@
-
 #[derive(Debug)]
 pub struct Rectangle {
     length: u32,
@@ -13,6 +12,10 @@ impl Rectangle{
 
 pub fn add_two(a: i32) -> i32{
     a + 2
+}
+
+pub fn greeting(name: &str) -> String{
+    format!("Hello")
 }
 
 #[cfg(test)]
@@ -38,5 +41,13 @@ mod tests {
     #[test]
     fn it_adds_two(){
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name(){
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was {}", result);
     }
 }
