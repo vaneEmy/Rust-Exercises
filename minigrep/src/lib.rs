9e9372f35934e54a28f3dbe4202d16fffe476c8f
vaneEmy <vanvanemily@gmail.com>
2018-05-18 16:51:01 -0500

    using the search function in the run function.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,8 +26,11 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-   
     f.read_to_string(&mut contents)?;
+
+    for line in search(&config.query, &contents){
+        println!("{}", line);
+    }
     
     Ok(())
 }
