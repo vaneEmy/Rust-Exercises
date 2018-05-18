cadb1eb4a294d022192dc0e6df38dce4427873cc
vaneEmy <vanvanemily@gmail.com>
2018-05-18 16:37:03 -0500

    Storing the lines that match so we can return them.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -33,11 +33,14 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
     for line in contents.lines(){
         if line.contains(query){
-
+            results.push(line);
         }
     }
+    results
 }
 
 #[cfg(test)]
@@ -48,12 +51,12 @@ mod test{
     fn one_result(){
         let query = "duct";
         let contents = "\n
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(
-            vec!["sage, fast, productive."], 
+            vec!["safe, fast, productive."], 
             search(query, contents)
             );
     }
