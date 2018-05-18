7f6e654a2958cc3bcaaf4ccd4dcfd19ccf83927e
vaneEmy <vanvanemily@gmail.com>
2018-05-18 16:15:16 -0500

    iterating through each line in contents.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -33,7 +33,9 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-    vec![]
+    for line in contents.lines(){
+        
+    }
 }
 
 #[cfg(test)]
