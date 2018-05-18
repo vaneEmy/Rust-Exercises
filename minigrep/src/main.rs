65f6db359494a65e77f587fee4faf18b758ffad3
vaneEmy <vanvanemily@gmail.com>
2018-05-18 17:39:24 -0500

    writting error messages to standard error instead of standard output using eprintln

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,13 +9,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     }) ;
 
 
     if let Err(e) = minigrep::run(config){
-        println!("Applicaion error: {}", e);
+        eprintln!("Applicaion error: {}", e);
         process::exit(1);
     }
 
