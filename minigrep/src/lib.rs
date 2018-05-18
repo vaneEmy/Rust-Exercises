50c56e444eceb2b427fd1b263f20a57e64e69828
vaneEmy <vanvanemily@gmail.com>
2018-05-18 16:13:55 -0500

    Defining just enough of the search function so the test will compile.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -32,6 +32,10 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    vec![]
+}
+
 #[cfg(test)]
 mod test{
     use super::*;
