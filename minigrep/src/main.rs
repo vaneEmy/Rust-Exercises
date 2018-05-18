21c17e8d528885141c24438f956ab176580e231a
vaneEmy <vanvanemily@gmail.com>
2018-05-18 14:54:15 -0500

    Refactoring parse_config to return an instance of a Config struct.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,12 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = parse_config(&args);
 
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -21,10 +21,14 @@ fn main() {
 
 }
 
+struct Config{
+    query: String,
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str){
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }
