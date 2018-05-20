2ec340015dc22971ca60bf3d5b89ee7d139c057a
vaneEmy <vanvanemily@gmail.com>
2018-05-20 18:28:38 -0500

    A main function with hardcoded values to simulate user input and random number generation.

--- closure_exercises/src/main.rs
+++ closure_exercises/src/main.rs
@@ -2,7 +2,13 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    println!("Hello, world!");
+    let simulated_user_specified_value =  10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32{
