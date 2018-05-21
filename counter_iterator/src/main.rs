c8b71fa516aad87a4fe92e775b43b68553c46e38
vaneEmy <vanvanemily@gmail.com>
2018-05-21 14:38:01 -0500

    testing the functionality of the next method implementation.

--- counter_iterator/src/main.rs
+++ counter_iterator/src/main.rs
@@ -24,3 +24,16 @@ impl Iterator for Counter{
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+    
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
