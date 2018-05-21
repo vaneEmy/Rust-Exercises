4f6edf8a640eecc4eb76eb993fbcf33df0f60336
vaneEmy <vanvanemily@gmail.com>
2018-05-21 15:06:15 -0500

    Changing the body of Config::new to use iterator methods.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -11,14 +11,18 @@ pub struct Config{
 
 impl Config{
     pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        let query = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
+        let filename = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+        
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
