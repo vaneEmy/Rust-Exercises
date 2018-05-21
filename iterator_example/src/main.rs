001f4a04396df50ca8386ee4fcc7e9eb1a3cafe2
vaneEmy <vanvanemily@gmail.com>
2018-05-21 14:20:01 -0500

    Using the filter method with a closure that captures shoe_size.

--- iterator_example/src/main.rs
+++ iterator_example/src/main.rs
@@ -1,3 +1,33 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe>{
+    shoes.into_filter().filter(|s| s.size == shoe.size).collect()
+}
+
+#[test]
+fn filters_by_size() {
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker")},
+        Shoe { size: 13, style: String::from("sandal")},
+        Shoe { size: 10, style: String::from("boot")},
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    assert_eq!(in_my_size,
+        vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 10, style: String::from("boot")},
+        ]
+    );
+}
+
+
+
 fn main() {
     let v1 = vec![1, 2, 3];
     let v1_iter = v1.iter();
