3e57a88e33cdf54563e23aff1389d876ae58244c
vaneEmy <vanvanemily@gmail.com>
2018-05-22 13:37:22 -0500

    Adding a test to a workspace and creating another one called add_two

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,7 +1,10 @@
 extern crate add_one;
 extern crate rand;
+extern crate add_two;
 
 fn main() {
     let num = 10;
     println!("Hello, world! {} plus one is {}!", num, add_one::add_one(num));
+    println!("{} plus two is {}!", num, add_two::add_two(num));
+
 }
