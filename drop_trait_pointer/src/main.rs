db53ad85d9dd47413da470a5fce5668d157d6b57
vaneEmy <vanvanemily@gmail.com>
2018-05-23 13:32:01 -0500

    A CustomSmartPointer struct that implements the Drop trait where we could ut our cleanup code.

---
+++ drop_trait_pointer/src/main.rs
@@ -0,0 +1,14 @@
+struct CustomSmartPointer{
+    data: String,
+}
+
+impl Drop for CustomSmartPointer{
+    fn drop(&mut self){
+        println!("Dropping customSmartPointer with data ´{}´ !", self.data);
+    }
+}
+fn main() {
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stugg") };
+    println!("CustomSmartPointers created.");
+}
