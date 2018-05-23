40395eeb91f1cf890b75323b3c5c028174f39fc4
vaneEmy <vanvanemily@gmail.com>
2018-05-23 15:09:56 -0500

    An attempt to implement a MockMessenger that isn't allowed by the borrow checker.

--- messenger/src/lib.rs
+++ messenger/src/lib.rs
@@ -35,8 +35,31 @@ impl<'a, T>LimitTracker<'a, T>
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    struct MockMessenger{
+        sent_messages: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger{
+            MockMessenger { sent_messages: vec![] }
+        }
+    }
+
+    impl Messenger for MockMessenger{
+        fn send(&self, message: &str){
+            self.sent_messages.push(String::from(message));
+        }
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn is_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
     }
 }
