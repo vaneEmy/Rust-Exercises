9fffdf82e42b478481ec12cbcd0c7125016242d0
vaneEmy <vanvanemily@gmail.com>
2018-05-24 14:17:54 -0500

    Attempting to use a vector created by the main thread in another thread.

--- thread_example/src/main.rs
+++ thread_example/src/main.rs
@@ -2,17 +2,11 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let v = vec![1, 2, 3];
+
     let handle =  thread::spawn(||{
-        for i in 1..10{
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..5{
-        println!("hi numbrer {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }    
-
+        println!("Here's a vector: {:?}", v);
+        });
+    
     handle.join().unwrap();
 }
