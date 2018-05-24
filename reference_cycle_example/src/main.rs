7c4e0a2a8e8b5f05c1876877f400e34c23b5b897
vaneEmy <vanvanemily@gmail.com>
2018-05-24 13:03:12 -0500

    Creating a reference cycle of two List values pointing to each other.

--- reference_cycle_example/src/main.rs
+++ reference_cycle_example/src/main.rs
@@ -9,7 +9,7 @@ enum List{
 }
 
 impl List {
-    fn tail(&self) -> Option<&refCell<Rc<List>>>{
+    fn tail(&self) -> Option<&RefCell<Rc<List>>>{
         match *self{
             Cons(_, ref item) => Some(item),
             Nil => None,
@@ -19,5 +19,22 @@ impl List {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail(){
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
 }
