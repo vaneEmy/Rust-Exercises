6d1cd28ce88517edbc2a52704c5a3fc92c3187c5
vaneEmy <vanvanemily@gmail.com>
2018-05-24 13:37:11 -0500

    Creating branch in an inner cope and examining strong and weak references counts.

--- tree_data_structure/src/main.rs
+++ tree_data_structure/src/main.rs
@@ -15,17 +15,38 @@ fn main() {
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf strong = {}, weak = {}",
+                Rc::strong_count(&leaf),
+                Rc::weak_count(&leaf),                 
+            );
+
+    {
+        // We clone the Rc<Node> in leaf and store that in branch,
+        // meaning the Node in leaf now has two owners: leaf and branch
+    
+        let branch = Rc::new(Node{
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+    
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!("branch strong = {}, weak = {}",
+                Rc::strong_count(&branch),
+                Rc::weak_count(&branch),                 
+            );
+        
+        println!("leaf strong = {}, weak = {}",
+                Rc::strong_count(&leaf),
+                Rc::weak_count(&leaf),                 
+            );
+    }
+
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-
-    // We clone the Rc<Node> in leaf and store that in branch,
-    // meaning the Node in leaf now has two owners: leaf and branch
-    let branch = Rc::new(Node{
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
-
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    
+    println!("leaf strong = {}, weak = {}",
+                Rc::strong_count(&leaf),
+                Rc::weak_count(&leaf),                 
+            );
 }
