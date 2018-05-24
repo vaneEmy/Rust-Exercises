f21ae61ea5fcef6a5acdebeac3cd23336024f0ac
vaneEmy <vanvanemily@gmail.com>
2018-05-24 14:20:14 -0500

    Using the move keyword to force a closure to take ownership of the values it uses.

--- thread_example/src/main.rs
+++ thread_example/src/main.rs
@@ -1,12 +1,11 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
     let v = vec![1, 2, 3];
 
-    let handle =  thread::spawn(||{
+    let handle =  thread::spawn(move ||{
         println!("Here's a vector: {:?}", v);
         });
-    
+
     handle.join().unwrap();
 }
