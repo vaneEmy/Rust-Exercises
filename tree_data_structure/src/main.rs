ffca464ce31c375b36118d0676867162eba9bc36
vaneEmy <vanvanemily@gmail.com>
2018-05-24 13:24:10 -0500

    A leaf node with a weak reference to its parent node branch.

--- tree_data_structure/src/main.rs
+++ tree_data_structure/src/main.rs
@@ -1,20 +1,31 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
 #[derive(Debug)]
 struct Node{
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn main() {
     let leaf = Rc::new(Node{
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    // We clone the Rc<Node> in leaf and store that in branch,
+    // meaning the Node in leaf now has two owners: leaf and branch
     let branch = Rc::new(Node{
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
