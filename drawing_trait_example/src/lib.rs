f1e23794cb07bd6d43a93f785b24beb4be43dbb5
vaneEmy <vanvanemily@gmail.com>
2018-05-25 16:21:04 -0500

    Implementing a run method on Screen that calls the draw method on each component.

--- drawing_trait_example/src/lib.rs
+++ drawing_trait_example/src/lib.rs
@@ -6,6 +6,14 @@ struct Screen {
     pub components: Vec<Box<Draw>>,
 }
 
+impl Screen {
+    pub fn run(&self){
+        for component in self.components.iter(){
+            component.draw();
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
