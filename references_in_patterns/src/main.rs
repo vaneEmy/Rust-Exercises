04c6c943d8062e213ceca1f2f7b646d417528225
vaneEmy <vanvanemily@gmail.com>
2018-05-28 14:25:34 -0500

    Creating a mutable reference to a value as part of a pattern using ref mut

--- references_in_patterns/src/main.rs
+++ references_in_patterns/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let robot_name = Some(String::from("Bors"));
+    let mut robot_name = Some(String::from("Bors"));
 
     match robot_name {
-        Some(ref name) => println!("Found a name: {}", name),
+        Some(ref mut name) => *name = String::from("Another name"),
         None => (),
     }
     println!("Robot_name is {:?}", robot_name);
