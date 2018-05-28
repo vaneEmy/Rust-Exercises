e13630c588e74727e88d8f814eb7eec74bec7f57
vaneEmy <vanvanemily@gmail.com>
2018-05-28 14:18:02 -0500

    Matching only the first and lasst values in a tuple and ignoring all other values.

--- ignoring_remaining_parts/src/main.rs
+++ ignoring_remaining_parts/src/main.rs
@@ -6,8 +6,15 @@ struct Point {
 
 fn main() {
     let origin = Point { x: 0, y: 0, z: 0};
+    let numbers = (2, 4, 6, 16, 32);
 
     match origin{
         Point {x, .. } => println!("x is {}", x), 
+    }
+
+    match numbers{
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        },
     }
 }
