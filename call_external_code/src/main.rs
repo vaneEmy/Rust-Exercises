116f6c6a1ad230fc2cecd9ecfa0e71a04a7328cd
vaneEmy <vanvanemily@gmail.com>
2018-05-28 15:44:23 -0500

    Making the call_from_c function accessible from C code.

--- call_external_code/src/main.rs
+++ call_external_code/src/main.rs
@@ -2,6 +2,11 @@ extern "C"  {
     fn abs(input: i32) -> i32;
 }
 
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from c");
+}
+
 fn main() {
     unsafe{
         println!("Absolute value f -3 according to C: {}", abs(-3));
