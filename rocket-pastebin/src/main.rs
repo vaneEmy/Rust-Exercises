3d7c555b24b04674d5f60f2a821027270ba6eb34
vaneEmy <vanvanemily@gmail.com>
2018-06-08 00:51:47 -0500

    Creating a index route.

--- rocket-pastebin/src/main.rs
+++ rocket-pastebin/src/main.rs
@@ -3,6 +3,23 @@
 
 extern crate rocket;
 
+#[get("/")]
+fn index() -> &'static str {
+    "   
+    USAGE
+
+    POST /
+    
+        accepts raw data in the body of the request and responds with a URL of
+        a page containing the body's content
+
+    GET /<id>
+        
+        retrieves the content for te paste with id ´<id>´
+    
+    "
+}
+
 fn main() {
-    rocket::ignite().launch();
+    rocket::ignite().mount("/", routes![index]).launch();
 }
