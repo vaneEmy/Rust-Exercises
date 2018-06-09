d471b8686946d56ab98981c58d93c502fbf21ae1
vaneEmy <vanvanemily@gmail.com>
2018-06-09 02:20:10 -0500

    Adding methods to update post.

--- diesel_demo/src/bin/write_post.rs
+++ diesel_demo/src/bin/write_post.rs
@@ -7,11 +7,11 @@ use std::io::{stdin, Read};
 fn main() {
     let connection = establish_connection();
 
-    println!("What would you like ypur title to be?");
+    println!("What would you like your title to be?");
     let mut title = String::new();
     stdin().read_line(&mut title).unwrap();
+    let title = title.trim_right(); // Remove the trailing newline
 
-    let title = &title[..(title.len() -1)];
     println!("\nOk Let's write {} (Press {} when finished)\n", title, EOF);
     let mut body = String::new();
     stdin().read_to_string(&mut body).unwrap();
