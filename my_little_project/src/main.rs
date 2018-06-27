8e2eb4cc79b61186068eecc8b68251f6540d3376
vaneEmy <vanvanemily@gmail.com>
2018-06-27 00:30:40 -0500

    Creating structs for the little project

--- my_little_project/src/main.rs
+++ my_little_project/src/main.rs
@@ -1,3 +1,34 @@
+#[derive(PartialEq, Debug)]
+enum TerrainGround {
+    Soil,
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum TerrainBlock {
+    Tree,
+    Soil,
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum Being {
+    Orc,
+    Human
+}
+
+struct Square {
+    gound: TerrainGround,
+    block: Option<TerrainBlock>,
+    beings: Option<Being>
+}
+
+struct Grid {
+    size: (usize, usize),
+    squares: Vec<Square>
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
