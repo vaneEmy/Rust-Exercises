7957c538af0b7b7c01dfb636d6f67d777ff8f3b8
vaneEmy <vanvanemily@gmail.com>
2018-06-27 00:39:37 -0500

    Adding structs.

--- my_small_project/src/main.rs
+++ my_small_project/src/main.rs
@@ -1,3 +1,33 @@
+#[derive(PartialEq, Debug)]
+enum TerrainGround {
+    Soil,
+    Stone,
+}
+
+#[derive(PartialEq, Debug)]
+enum TerrainBlock {
+    Tree,
+    Soil,
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum Being {
+    Orc,
+    Human,
+}
+
+struct Square {
+    ground: TerrainGround,
+    block: Option<TerrainBlock>,
+    beings: Option<Being>
+}
+
+struct Grid {
+    size: (usize, usize),
+    squares: Vec<Square>
+}
+
 fn main() {
     println!("Hello, world!");
 }
